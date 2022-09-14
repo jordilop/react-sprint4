1912b0195bdb111cb32ec43ab9ca7708dbf1c446
Jordi <jordilop@gmail.com>
2022-09-15 00:02:35 +0200

    Exercici 1

--- src/films.js
+++ src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  const result = array.map(item => item.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
