437fa926930e1251a20b68e9189320b501815e1c
Jordi <jordilop@gmail.com>
2022-09-20 16:53:44 +0200

    Exercici 4

--- src/films.js
+++ src/films.js
@@ -1,23 +1,27 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  const result = array.map(item => item.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const result = array.filter(item => item.director == director);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  const result = getMoviesFromDirector(array, director);
+  const average = Number((result.reduce((previousValue, currentValue) => previousValue.score + currentValue.score) / result.length).toFixed(2));
+  return average;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  const result = array.map(item => item.title).sort();
+  return result.slice(0, 20);
 }
 
 // Exercise 5: Order by year, ascending
